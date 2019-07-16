3bd9cac6a0c824159220db2079c79f389e1aef7e
Shreya Randive <shreyarandive28@gmail.com>
2019-07-15 22:53:38 -0400

    Add main VC

---
+++ WhereIsMyFood?/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  WhereIsMyFood?
+//
+//  Created by Shreya Randive on 7/15/19.
+//  Copyright © 2019 Shreya Randive. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 4.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    func setupView() {
+         self.layer.cornerRadius = cornerRadius
+    }
+}
